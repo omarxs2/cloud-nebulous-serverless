cc0bb0d28b9422c5188e0627ef7a42f4dde75f0e
omar_xs2 <omar.soliman306@gmail.com>
2022-03-29 15:44:55 +0300

    initial commit

--- terraform/gcp.tf
+++ terraform/gcp.tf
@@ -8,19 +8,19 @@ terraform {
 }
 
 provider "google" {
-    project = "devops-343007"
+    project = "omar-devops"
 }
 
 # Cloud Run
 
 resource "google_cloud_run_service" "cloudrun" {
-  name     = "cloudrun-service-test-1"
+  name     = "cloudrun-service-test-2"
   location = "us-central1"
 
   template {
     spec {
       containers {
-        image = "gcr.io/devops-343007/translate-app:latest"
+        image = "gcr.io/omar-devops/translate-app-img:latest"
       }
     }
   }
@@ -29,4 +29,20 @@ resource "google_cloud_run_service" "cloudrun" {
     percent         = 100
     latest_revision = true
   }
+}
+
+data "google_iam_policy" "noauth" {
+  binding {
+    role = "roles/run.invoker"
+    members = [
+      "allUsers",
+    ]
+  }
+}
+
+resource "google_cloud_run_service_iam_policy" "noauth" {
+  location    = google_cloud_run_service.cloudrun.location
+  project     = google_cloud_run_service.cloudrun.project
+  service     = google_cloud_run_service.cloudrun.name
+  policy_data = data.google_iam_policy.noauth.policy_data
 }
